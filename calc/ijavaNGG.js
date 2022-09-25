2a040c186b07b4da96b04aa6897cfe7802ac7726
Alektryon <84470810+Alektryon@users.noreply.github.com>
2022-09-25 14:21:49 +0100

    Update ijavaNGG.js

--- calc/ijavaNGG.js
+++ calc/ijavaNGG.js
@@ -35,7 +35,8 @@ function Populate_MenuBar() {
 	hStr += '<div class="MenuLink"><a href="javascript:Open_Options()">Options</a></div>  |  '
 	hStr += '<div class="MenuLink"><a href="https://github.com/Alektryon/gemcon">GitHub (changelog)</a></div>  |  '
 	hStr += '<div class="MenuLink"><a href="https://gematriaresearch.blogspot.com?m=0">Gematria Research</a></div>'
-
+	hStr += '<BR>'
+	hStr += '<div class="MenuLink">(Code based on <a href="http://www.gematrinator.com/calculator/index.php">Gematrinator.com</a>)</div>'
 	hStr += '<BR></center>'
 	mSpot.innerHTML = hStr
 }
